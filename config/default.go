55313bb789f3c4a178c0b3d7dc39e1404d522236
Jernej Kos <jernej@kos.mx>
2023-10-19 09:40:03 +0200

    feat: Automatically migrate old chain contexts for known networks

--- config/default.go
+++ config/default.go
@@ -8,3 +8,12 @@ import (
 var Default = Config{
 	Networks: config.DefaultNetworks,
 }
+
+// OldNetworks contains information about old versions (e.g. chain contexts) of known networks so
+// they can be automatically migrated.
+var OldNetworks = map[string][]string{
+	// Testnet.
+	"testnet": {
+		"50304f98ddb656620ea817cc1446c401752a05a249b36c9b90dba4616829977a", // 2022-03-03
+	},
+}

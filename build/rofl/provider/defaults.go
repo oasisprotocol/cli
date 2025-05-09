7bb41039394f86b3ab95bc6aac2229c4e89910ce
Matevž Jekovec <matevz@oasisprotocol.org>
2025-05-10 00:35:48 +0200

    cmd/rofl/deploy: Fix --provider behaviour

--- build/rofl/provider/defaults.go
+++ build/rofl/provider/defaults.go
@@ -1,8 +1,20 @@
 package provider
 
-// DefaultSchedulerApp contains the default scheduler app IDs for each network/paratime.
-var DefaultSchedulerApp = map[string]map[string]string{
-	"testnet": {
-		"sapphire": "rofl1qrqw99h0f7az3hwt2cl7yeew3wtz0fxunu7luyfg",
+import "github.com/oasisprotocol/oasis-sdk/client-sdk/go/config"
+
+// RoflServices contains per network/ParaTime default ROFL services.
+type RoflServices struct {
+	// Scheduler is a ROFL app ID of the preferred scheduler app.
+	Scheduler string
+
+	// Provider is the address of the preferred provider.
+	Provider string
+}
+
+// DefaultRoflServices contains default built-in ROFL services for all supported networks/ParaTimes.
+var DefaultRoflServices = map[string]RoflServices{
+	config.DefaultNetworks.All["testnet"].ParaTimes.All["sapphire"].ID: {
+		Scheduler: "rofl1qrqw99h0f7az3hwt2cl7yeew3wtz0fxunu7luyfg",
+		Provider:  "oasis1qp2ens0hsp7gh23wajxa4hpetkdek3swyyulyrmz",
 	},
 }
